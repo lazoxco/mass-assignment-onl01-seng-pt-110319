308b75f3f8986ee581e356dccaebf4958769a5c9
Nory Lazo <nlazo@icloud.com>
2019-12-16 05:43:47 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,8 @@
 class Person
   #your code here
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :plant_length, :pant_width
+  
+  def initialize(attributes)
+    
+  end
 end
